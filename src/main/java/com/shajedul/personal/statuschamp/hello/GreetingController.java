0ca25fcd38a75655ec5f291eb61d186f2ba85890
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-05-08 17:02:06 +0600

    Used @RestController instead of MVC @controller for the same functionality

--- src/main/java/com/shajedul/personal/statuschamp/hello/GreetingController.java
+++ src/main/java/com/shajedul/personal/statuschamp/hello/GreetingController.java
@@ -1,27 +1,22 @@
 package com.shajedul.personal.statuschamp.hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/hello")
-    @ResponseBody
     public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     public String index() {
         return "Greetings from Statuschamp!";
     }
