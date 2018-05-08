f882ad95ea4ad341b2e07568cf31145563563b90
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-05-08 15:31:47 +0600

    Initial commit. Spring Boot skeleton project with JPA and MySQL. Supplied Datasource configuration for MySQL

---
+++ src/main/java/com/shajedul/personal/statuschamp/StatuschampApplication.java
@@ -0,0 +1,12 @@
+package com.shajedul.personal.statuschamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatuschampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatuschampApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shajedul/personal/statuschamp/StatuschampApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shajedul.personal.statuschamp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatuschampApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
